a5a1ed8c7da08aeedeaa2f6e5648935bd7df06ec
feliperuppel <feliperuppel@gmail.com>
2019-08-08 13:53:58 -0300

    Top part almost there

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,13 +1,13 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'Detailpage.dart';
 import 'PopularMovie.dart';
 import 'PopularMovieData.dart';
+import 'model/animeModel.dart';
 import 'movieslider.dart';
-import 'package:netflix_clone/model/moviesmodel.dart';
-
-
 
 class Homepage extends StatefulWidget {
   @override
@@ -18,118 +18,116 @@ class _HomepageState extends State<Homepage>
     with SingleTickerProviderStateMixin {
   Animation animation;
   AnimationController animationController;
+  Future<List<Anime>> loadedAnimes;
+  Future<List<Anime>> loadedBestAnimes;
+
   @override
   void initState() {
-   // super.initState();
+    super.initState();
     animationController =
-        new AnimationController(duration: Duration(seconds:20), vsync: this);
-    animation =
-        IntTween(begin: 0, end: photos.length-1 ).animate(animationController)
-    ..addListener((){
-      setState(() {
-        index=animation.value;
-      });
-    });
-
+        new AnimationController(duration: Duration(seconds: 20), vsync: this);
     animationController.repeat(period: Duration(seconds: 20));
+
+    loadedAnimes = fetchAnimes('http://one.zetai.info/api/animes/recentes');
+    loadedBestAnimes = fetchAnimes('http://one.zetai.info/api/animes/recentes');
   }
 
+  Future<List<Anime>> fetchAnimes(String url) async {
+    print("fetchAnimes : Rest GET ---------> " + url);
+    final response = await http.get(Uri.encodeFull(url),
+        headers: {'Content-type': 'application/json; charset=utf-8'});
+    if (response.statusCode == 200) {
+      Iterable list = json.decode(response.body);
+      animation = IntTween(
+              begin: 0,
+              end:
+                  3) //TODO Replace 'end' with the actual size of the result list.length -1
+          .animate(animationController)
+            ..addListener(() {
+              setState(() {
+                index = animation.value;
+              });
+            });
+      return list.map((model) => Anime.fromJson(model)).toList();
+    } else {
+      throw Exception(
+          'Failed do load Animes'); //TODO Better understanding of Exception handling needed
+    }
+  }
 
   int index = 0;
-  List<String> text=['John Wick 3',
-    'Endgame',
-    'Shazam',
-    'Captain Marvel',
-    'Detective Pikachu',
-    'Dark Phoenix',
-    'Spiderman:Far from Home',
-    'Once upon a time in Hollywood',
-    'Hobbs & Shaw'];
-  List<String> photos = [
-    'https://boundingintocomics.com/files/2019/03/2019.03.21-05.04-boundingintocomics-5c93c41d43700.png'
-    ,'https://initiate.alphacoders.com/images/100/cropped-1920-1080-1003220.png?3768'
-    ,
-    'https://in.bookmyshow.com/entertainment/wp-content/uploads/2019/04/Shazam_960x540.jpg'
-    ,
-    'https://cdn.vox-cdn.com/thumbor/rW8rg4GByXrm84_qqn9sUgqA3w0=/0x145:1688x1809/1200x480/filters:focal(726x441:996x711)/cdn.vox-cdn.com/uploads/chorus_image/image/61461869/captain_marvel_poster_1688.1537366019.jpg'
-    ,'https://initiate.alphacoders.com/images/971/cropped-1920-1080-971284.jpg?8655'
-    ,
-    'https://s3-us-east-2.amazonaws.com/redefined/wp-content/uploads/2019/02/28014226/dark-phoenix-DarkPhoenix_OneSheet2_rgb.jpg'
-    ,
-    'http://t2.gstatic.com/images?q=tbn:ANd9GcQ-hK4oBcmE5BZk71a4wdZ0xe7w2aru9fUtTTcT_kDZDrYGe4E8'
-    ,
-    'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1687,w_3000,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1558479602/190520-Porton-Once-Upon-a-Time-in-Hollywood-tease_fla7w4'
-    ,
-    'https://scontent-sin6-2.cdninstagram.com/vp/90b6f0d13a4306003da98cd10d82cea4/5D1D5B52/t51.2885-15/e35/50618325_2328246480765217_2306485043007719624_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&se=7&ig_cache_key=MTk3ODA5Nzg1NDQwMDQ1NzI0Mg%3D%3D.2'
-
-  ];
-
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    animationController.dispose();
-  }
-
-
-  @override
-
   Widget build(BuildContext context) {
-    Popmovies(PopularList plist) =>
-        InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => Detailpage(list: plist,)),
-              );
-            },
-            child: PopularMovie(
-              image: plist.image,
-              name: plist.name,
-              rating: plist.rating,
-
-            ));
-    bestm(BMovies movie)=>TopMovies(
-      image: movie.image,
-    );
+    popMovies(PopularList plist) => InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Detailpage(
+                      list: plist,
+                    )),
+          );
+        },
+        child: PopularMovie(
+          image: plist.image,
+          name: plist.name,
+          rating: plist.rating,
+        ));
 
     final popularscroll = Container(
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         physics: BouncingScrollPhysics(),
         child: Row(
-          //  children:bmovies1.map((movie)=>bestm(movie)).toList()
-          children: populartlist.map((pl) => Popmovies(pl)).toList(),
+          children: populartlist.map((pl) => popMovies(pl)).toList(),
         ),
       ),
     );
-    final scroll=Container(
-      child:SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        physics: BouncingScrollPhysics(),
-        child: Row(
-          //  children:bmovies1.map((movie)=>bestm(movie)).toList()
-          children: bmovies1.map((movie)=>bestm(movie)).toList(),
-        ),
-      ) ,
-    );
+    final scroll = Container(
+        child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            child: FutureBuilder<List<Anime>>(
+              future: loadedBestAnimes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  bestAnimes(Anime anime) => TopMovies(
+                        image: anime.imagem,
+                      );
+                  Anime anime = snapshot.data[index];
+                  return Row(
+                    children: snapshot.data
+                        .map((movie) => bestAnimes(movie))
+                        .toList(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                // By default, show a loading spinner.
+                return CircularProgressIndicator();
+              },
+            )));
 
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            "Netflix",
-            style: TextStyle(color: Colors.red,fontSize: 28),
+            "Sushinime POC",
+            style: TextStyle(color: Colors.orange, fontSize: 28),
           ),
           backgroundColor: Colors.black,
           //    leading:Icon(Icons.notifications,color: Colors.red,) ,
           actions: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.notifications,color: Colors.red,size:25,),
+              child: Icon(
+                Icons.notifications,
+                color: Colors.orange,
+                size: 25,
+              ),
             )
           ],
           //  toolbarOpacity: 0,
@@ -139,32 +137,50 @@ class _HomepageState extends State<Homepage>
           child: Column(
             children: <Widget>[
               Container(
-                color: Colors.black,
+                  color: Colors.black,
+                  child: FutureBuilder<List<Anime>>(
+                    future: loadedAnimes,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        Anime anime = snapshot.data[index];
+                        return Column(
+                          children: <Widget>[
+                            ImageData(anime.imagem, anime.nome),
+                            scroll
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
 
-                child: Column(
-                  children: <Widget>[
-                    ImageData(photos[index], text[index]),
-                    scroll
-                    //  TopMovies()
-
-                  ],
-                ),
-              ),
+                      // By default, show a loading spinner.
+                      return CircularProgressIndicator();
+                    },
+                  )),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
                   // crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Continue Watching",
-                      style: TextStyle(fontWeight: FontWeight.w900,
-                          fontSize: 16, color: Colors.white),),
-                    SizedBox(width: 180,),
-                    Text("See All",
-                      style: TextStyle(fontWeight: FontWeight.w900,
-                          fontSize: 16, color: Colors.red),)
+                    Text(
+                      "Continue Watching",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: 180,
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: Colors.orange),
+                    )
                   ],
                 ),
-
               ),
               netflixslider(),
               Padding(
@@ -172,32 +188,48 @@ class _HomepageState extends State<Homepage>
                 child: Row(
                   // crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Popular On Netflix",
-                      style: TextStyle(fontWeight: FontWeight.w900,
-                          fontSize: 16, color: Colors.white),),
-                    SizedBox(width: 180,),
-                    Text("See All",
-                      style: TextStyle(fontWeight: FontWeight.w900,
-                          fontSize: 16,color: Colors.red),)
+                    Text(
+                      "Popular On Netflix",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: 180,
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: Colors.orange),
+                    )
                   ],
                 ),
-
               ),
               popularscroll
-
             ],
           ),
         ));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
   }
 }
 
 class ImageData extends StatelessWidget {
   final String image;
   final String name;
+
   ImageData(this.image, this.name);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       height: 180,
       decoration: BoxDecoration(
@@ -211,16 +243,44 @@ class ImageData extends StatelessWidget {
         child: Text(
           name,
           style: TextStyle(
-            backgroundColor: Colors.black12,
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
+//            backgroundColor: Colors.black26,
+            color: Colors.orangeAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(5.0, 0.0),
+              ),
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(-5.0, 0.0),
+              ),
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(0.0, -5.0),
+              ),
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(0.0, 5.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-class TopMovies extends StatelessWidget{
- final String image;
-  TopMovies({Key key,this.image});
+
+class TopMovies extends StatelessWidget {
+  final String image;
+
+  TopMovies({Key key, this.image});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -236,20 +296,14 @@ class TopMovies extends StatelessWidget{
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(12.0),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0.5, 1.0),
-                blurRadius: 5,
-                color: Colors.white
-              )
-            ]
-          ),
-
-
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0.5, 1.0),
+                    blurRadius: 5,
+                    color: Colors.white)
+              ]),
         ),
       ),
     );
   }
-
 }
-
---
+++ lib/model/animeModel.dart
@@ -0,0 +1,90 @@
+
+
+import 'package:flutter/material.dart';
+
+class Anime extends StatelessWidget{
+  final int id;
+  final String nome;
+  final String desc;
+  final bool status;
+  final String imagem;
+  final String ano;
+  final String categoria;
+  final int rank;
+
+  Anime({this.id, this.nome, this.desc, this.status, this.imagem, this.ano, this.categoria, this.rank});
+
+  factory Anime.fromJson(Map<String, dynamic> json) {
+    return Anime(
+        id: json['Id'],
+        nome: json['Nome'],
+        desc: json['Desc'],
+        status: json['Status'],
+        imagem: json['Imagem'],
+        ano: json['Ano'],
+        categoria: json['Categoria'],
+        rank: json['Rank']
+    );
+  }
+
+//  @override
+//  String toString() {
+//
+//    return "id : " + id.toString() + "\n"
+//         + "nome : " + nome.toString() + "\n"
+//        + "desc : " + desc.toString() + "\n"
+//        + "status : " + status.toString() + "\n"
+//        + "imagem : " + imagem.toString() + "\n"
+//        + "ano : " + ano.toString() + "\n"
+//        + "categoria : " + categoria.toString() + "\n"
+//        + "rank : " + rank.toString() + "\n";
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 140,
+              width: 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(imagem),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(6.0),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0.5, 1.0),
+                        blurRadius: 5,
+                        color: Colors.white)
+                  ]),
+            ),
+            Container(
+              //width: 100,
+              //height: 40,
+              child: Text(
+                nome,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+            ),
+            Text(
+              "Rank ${rank}",
+              style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
